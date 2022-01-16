27467c41db4696bbbe2baec0a6fbdbf95d084db4
Geovanne Borges <geovanneborges23@gmail.com>
2022-01-16 13:05:06 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const resultado=document.querySelector( '.result' )
+const confirmar= document.querySelector('#igual')
+
+function clean(){
+    resultado.innerHTML=' '
+}
+
+function insert(valor){
+    resultado.innerHTML +=valor
+}
+function apagar(){
+    if( resultado.textContent ){
+        let result= document.getElementById('resultado').innerHTML
+        resultado.innerHTML=result.substring(0, result.length-1)
+    }
+}
+function conclui(){
+    if(resultado.textContent != 'erro'){
+        document.getElementById('resultado').innerHTML = eval(resultado.innerHTML)
+    }
+}
